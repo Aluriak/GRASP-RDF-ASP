07a87aef4a0c0c7a8c8c641e4dd338f02088c56b
Aluriak <lucas.bourneuf@laposte.net>
2016-02-27 20:05:10 +0100

    main: use the new API + CLI improvement

--- grasp/__main__.py
+++ grasp/__main__.py
@@ -3,14 +3,17 @@ usage:
     __main__.py [--help]
     __main__.py convert <input_RDF_filename> <output_filename> [options]
     __main__.py retrieve <database_URI> <input_SPARQL_query> <output_filename> [options]
-    __main__.py query <input_ASP_data> <input_ASP_query> [options]
+    __main__.py query <input_ASP_query> <input_ASP_data> [options]
 
 options:
-    -h, --help          print this message
-    --keep-prev-asp     append the new data to local db instead of replace it
-    --keep-blanks       don't filter out triplets containing any blank data
-    --decompose         decompose more ASP triplets, allowing finer treatments
-    --no-uri            remove URIs from objects values
+    -h, --help            print this message
+    --keep-prev-asp       append the new data to local db instead of replace it
+    --keep-blanks         don't filter out triplets containing any blank data
+    --decompose           decompose more ASP atoms, allowing finer treatments
+    --no-uri              remove URIs from atoms values
+    --atom-name=STR       name of atoms pushed in ASP db
+    --gringo-options=STR  options send to the grounder
+    --clasp-options=STR   options send to the solver
 
 convert:
     Converts given RDF file in an ASP readable file.
@@ -18,22 +21,19 @@ convert:
 retrieve:
     Queries given database with given SPARQL query, convert results in ASP
     readable format and put it in given output file.
-    Note that the SPARQL query should expose the 3 variables object,
-    relation and subject.
 
 query:
     Performs the ASP query on the ASP readable format.
     Print the output in stdout.
 
 """
-import sys
 from functools import partial
 
 from docopt import docopt
 
 from . import rdftoasp
-from . import query
 from . import sparql
+import grasp
 
 
 if __name__ == '__main__':
@@ -43,32 +43,29 @@ if __name__ == '__main__':
     keep_blank_nodes = args['--keep-blanks']
     decompose_atom = args['--decompose']
 
-    # curry the postprocess function that humanize the values
-    value_postprocess = partial(rdftoasp.humanized, no_uri=args['--no-uri'])
+    # curry the postprocess function that convert the values
+    atom_converter = partial(rdftoasp.aspified, no_uri=args['--no-uri'],
+                              decompose=decompose_atom,
+                              atom_name=args['--atom-name'], end='.\n')
 
-    # perform the asked treatment
+    # perform the targeted treatment
     if args['convert']:
-        rdftoasp.file_to_file(args['<input_RDF_filename>'],
-                              args['<output_filename>'],
-                              atom_processed=value_postprocess,
-                              keep_previous_data=keep_prev_asp,
-                              keep_blank_nodes=keep_blank_nodes,
-                              decompose_atom=decompose_atom)
+        grasp.convert(args['<input_RDF_filename>'],
+                      args['<output_filename>'],
+                      erase=not keep_prev_asp,
+                      converter=atom_converter,
+                      keep_blank_nodes=keep_blank_nodes)
 
     elif args['retrieve']:
-        db_uri = args['<database_URI>']
-        sparql_query = args['<input_SPARQL_query>']
-        output_filename = args['<output_filename>']
-        of_mode = 'a' if keep_prev_asp else 'w'
-        with open(sparql_query) as qf, open(output_filename, of_mode) as of:
-            for triplet in sparql.query(db_uri, qf.read()):
-                of.write(rdftoasp.triplet_to_atom(triplet,
-                                                  processed=value_postprocess))
+        grasp.retrieve(args['<database_URI>'], args['<input_SPARQL_query>'],
+                       args['<output_filename>'], erase=not keep_prev_asp,
+                       converted=atom_converter)
 
     elif args['query']:
-        file_data = args['<input_ASP_data>']
-        file_query = args['<input_ASP_query>']
-        answers = query.answers(file_query, file_data)
+        answers = grasp.query(args['<input_ASP_query>'],
+                              args['<input_ASP_data>'],
+                              gringo_options=args['--gringo-options'],
+                              clasp_options=args['--clasp-options'])
         if len(answers) == 0:
             print('No answers !')
         for idx, answer in enumerate(answers):
