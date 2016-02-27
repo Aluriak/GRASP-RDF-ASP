41e05e0c3672bec6450c23d64e36643f2f0495b9
Aluriak <lucas.bourneuf@laposte.net>
2016-02-27 20:02:57 +0100

    query: receive gringo and clasp options

--- grasp/query.py
+++ grasp/query.py
@@ -7,6 +7,7 @@ from . import solving
 
 def humanized(model):
     """Return the given model, humanized"""
+    raise NotImplementedError
     for atom in model:
         pass
 
@@ -28,6 +29,10 @@ def arg(atom):
         return tuple()
 
 
-def answers(file_query, file_data):
-    """Yield answers of RDF triplets found in data that answer the query"""
-    return solving.model_from('', [file_query, file_data])
+def answers(file_query, file_data, gringo_options=None, clasp_options=None):
+    """Yield answers of ASP query on data"""
+    return solving.model_from(
+        '', [file_query, file_data],
+        gringo_options=gringo_options if gringo_options else '',
+        clasp_options=clasp_options if clasp_options else ''
+    )
